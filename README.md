91cd0342dadd3fffa3aead325871e24af2f4c750
Larissa Barra <lari_conde94@hotmail.com>
2014-12-04 19:37:16 +0000

    readapting to local

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ COMM049CW
 Coursework for the module COMM049 - HTML5 & CSS3 for Mobile Applications
 
 This coursework consists of a website to support tourists.
+It allows one to save places and addresses nearby, according to the selected theme, and access this data offline.
--- app.js
+++ app.js
@@ -11,22 +11,19 @@ var express = require('express'),
 var app = express();
 var __dirname = '.';
 
-// all environments
+//server options
 app.set('port', 8888);
+app.use(express.logger('dev'));
+//views
 app.set('views', __dirname + '/views/');
 app.set('view engine', 'html');
-app.use(express.logger('dev'));
 app.use(express.bodyParser());
-app.use(express.methodOverride());
+//required for sessions
 app.use(express.cookieParser('your secret here'));
 app.use(express.session());
+//routes
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
-
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
 
 app.get('/', routes.index);
 app.get('/view', routes.view);
@@ -34,6 +31,21 @@ app.post('/view', routes.view);
 app.get('/logout', routes.logout);
 app.get('/searches', routes.searches);
 
+//dealing with cache - offline.appcache gets these paths
+app.get('/:file', function(request, response){
+    response.sendfile(__dirname + '/views/' + request.params.file);
+});
+app.post('/:file', function(request, response){
+    response.sendfile(__dirname + '/views/' + request.params.file);
+});
+app.get('/public/:resource/:file', function(request, response){
+    response.sendfile(__dirname + '/public/' + request.params.resource + "/" + request.params.file);
+});
+app.get('/:resource/:file', function(request, response){
+    response.sendfile(__dirname + '/public/' + request.params.resource + "/" + request.params.file);
+});
+
+
 http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+    console.log('Express server listening on port ' + app.get('port'));
 });
